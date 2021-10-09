86ff98e80a0a38a04b2f9d28a2b97634e5ccd460
haticeakyel <56586646+haticeakyel@users.noreply.github.com>
2021-10-10 00:29:14 +0300

    created allowLocation page

---
+++ lib/views/allowLocation.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AllowLocation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: [
+          new Container(
+            height: 300.0,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/location.png"),
+                  fit: BoxFit.cover),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0, left: 65.0),
+            child: Text(
+              "Hi! Nice to meet you",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 10.0, left:30.0, right: 30.0),
+              child: Text(
+                "Please allow your location to continue using app",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 60.0, left: 65.0, right: 65.0),
+            child: FlatButton(
+              minWidth: 320.0,
+              height: 50.0,
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                      color: Colors.orange, width: 1, style: BorderStyle.solid),
+                  borderRadius: BorderRadius.circular(20)),
+              onPressed: () {},
+              child: Text(
+                "Use current location",
+                style: TextStyle(
+                  color: Colors.orange,
+                  fontSize: 25.0,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
